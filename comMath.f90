431420ef1a029cbd28a22373efea69cdad2bccb4
temirbaevabermet <150426617+temirbaevabermet@users.noreply.github.com>
2024-09-13 13:39:39 +0600

    float16 to float64

--- comMath.f90
+++ comMath.f90
@@ -1,15 +1,15 @@
 program machine_epsilon
     implicit none
-    real :: epsilon, test_val
+    real (kind=8) :: epsilon, test_val
 
-    epsilon = 1.0
+    epsilon = 1.0_8
 
-    do while (1.0 + epsilon > 1.0)
+    do while (1.0_8 + epsilon > 1.0_8)
         test_val=epsilon
-        epsilon = epsilon / 2.0
+        epsilon = epsilon / 2.0_8
     end do
 
-    epsilon = epsilon * 2.0
+    epsilon = epsilon * 2.0_8
 
     print *, 'Machine epsilon: ', test_val
 
