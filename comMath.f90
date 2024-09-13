d192a709822e2a536ce90e73831f3ce940d42ec4
temirbaevabermet <150426617+temirbaevabermet@users.noreply.github.com>
2024-09-13 14:19:51 +0600

    Second task foat16 to float64

--- comMath.f90
+++ comMath.f90
@@ -1,17 +1,17 @@
 program machine_epsilon
     implicit none
-    real :: epsilon, test_val
+    real (kind=8) :: epsilon, test_val
 
-    epsilon = 1.0
+    epsilon = 1.0_8
 
     do
-        if (1.0 + epsilon <= 1.0) then
+        if (1.0_8 + epsilon <= 1.0_8) then
             exit
         end if
-            epsilon=epsilon/2.0
+            epsilon = epsilon / 2.0_8
     end do
 
-    epsilon = epsilon * 2.0
+    epsilon = epsilon * 2.0_8
 
     print *, 'Machine epsilon: ', epsilon
 end program machine_epsilon
