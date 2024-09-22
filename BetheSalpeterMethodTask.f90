304e363005080a6b07e1840b962341fc617ac4fc
temirbaevabermet <150426617+temirbaevabermet@users.noreply.github.com>
2024-09-22 18:05:30 +0600

    Add files via upload

---
+++ BetheSalpeterMethodTask.f90
@@ -0,0 +1,24 @@
+program sinIntegral
+    real :: a, b
+    integer :: M 
+    integer :: i
+    real :: S, r, rN, pi
+    
+    M = 1000
+    S = 0
+    i = 0
+    pi = 4.D0*DATAN(1.D0)
+    a = pi
+    b = (3*pi)/2
+    
+    do while (i < M)
+        call random_number(rN)
+        r = (b - a) * rN + a
+        S = sin(r) * r + S 
+        i = i + 1 
+        print *, 'Iter №', i ,': S =',S/i
+    end do
+    
+    print *, 'Last: Total Int = ',S/M 
+
+end program sinIntegral
